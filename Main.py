a8353638c73ce802c70d2c25af2cc434c7893f26
BigNAYO <smallzcashot@gmail.com>
2025-05-12 15:32:11 +0100

    First commit

---
+++ Main.py
@@ -0,0 +1,15 @@
+Class Test
+def_init_(self, a, b):
+    self.a = a
+    self.b = b
+
+    def _str_(self):
+        return f"Test(a={self.a}, b={self.b})"
+
+        def add(self):
+            return self.a + self.b
+            def subtract(self):
+                return self.a -self.b
+                def multiply(self):
+                    return self.a* self.b
+                    
---
+++ Main.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Employee(
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VANCHAR(50),
+    Department VARCHAR(50),
+    Salary DECIMAL9(10, 2)
+);
+
+CREATE TABLE IF NOT EXISTS Department(
+    departmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(50),
+    Location  vanchar(50
+    );
+    )
+)
